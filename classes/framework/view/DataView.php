a44aabdc128b42a54fa457ae97e46d4bb4507eeb
shenzhe <shenzhe163@gmail.com>
2013-01-18 17:15:22 +0800

    add 自适应dataview
    支持 json, xml, amf, bin, html

--- classes/framework/view/DataView.php
+++ classes/framework/view/DataView.php
@@ -2,48 +2,35 @@
 
 namespace framework\view;
 
-use framework\core\IView;
+use framework\view;
+
 
 /**
- * amf视图，向用户输出amf
+ * 自适应view
+ *
+ * @package view
  */
-class AMFView implements IView {
-
-    private $model;
-
-    public function __construct($model) {
-        $this->model = $model;
+class DataView {
+    
+    public static function getView($model = null, $displayMode='json', $fileName=null) {
+        switch ($displayMode) {
+            case 'bin':
+                return new MsgpackView($model);
+                break;
+            case 'amf':
+                return new AMFView($model);
+                break;
+            case 'xml':
+                return new XMLView($model);
+                break;
+            case 'html':
+                return new SmartyView($model, $fileName);
+            default:
+                return new JSONView($model);
+        }
     }
 
-    /**
-     * 获取数据
-     *
-     * @return mixed
-     */
-    public function getModel() {
-        return $this->model;
-    }
 
-    /**
-     * 设置数据
-     *
-     * @param mixed $model
-     */
-    public function setModel($model) {
-        return $this->model = $model;
-    }
 
-    public function getXml() {
-        
-    }
-
-    /**
-     * 展示视图
-     *
-     */
-    public function display() {
-        \header("Content-Type: text/xml; charset=utf-8");
-        echo \amf3_encode($this->model);
-    }
 
 }
